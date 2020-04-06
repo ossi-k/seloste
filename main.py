1034dda11baa21ac0fe5c430d02320518e751403
ossi-k <56500693+ossi-k@users.noreply.github.com>
2020-04-06 17:25:03 +0300

    Add files via upload
    oikee toimiva uusin versio :)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def tee_seloste(data):
     #alustetaan muuttujia
     tulostus = ""
-    toimialat_sarakkeille = {}
-    seloste_toimialoille = {}
+    seloste_sarjoille = {}
+    sarjat_sarake_nimi = {}
+    muuttujat_sarake_nimi = {}
 
     #luetaan data
     data = open(data, encoding="latin-1")
@@ -15,63 +16,78 @@ def tee_seloste(data):
             y = y - 1
         y += 1
 
-    # päätellään muuttuja ja poistetaan otsikkorivi
-    eka_rivi = lines[0].split(" ")
-    muuttuja = eka_rivi[3][:11]
-    lines.pop(0)
+    # päätellään sarjojen nimet ja poistetaan otsikkorivi
+    lines.pop(0) #poistetaan tiedoston otsikkorivi
+    lines.pop(0)#poistetaan laskentajoukon otsikkorivi
+    sarjojen_nimet_rivi = lines[0].split(";")
+    sarjan_nimi = ""
+    for i in range(3, len(sarjojen_nimet_rivi)):
+        if sarjojen_nimet_rivi[i] != "":
+            sarjan_nimi = sarjojen_nimet_rivi[i]
+        sarjat_sarake_nimi[i] = sarjan_nimi
+    lines.pop(0) #poistetaan sarjojen otsikkorivi
+    lines.pop(0) #poistetaan muuttujien lyhenne-rivi
 
+    #alustetaan seloste-hajautustaulu
+    for i in range(len(sarjat_sarake_nimi)):
+        seloste_sarjoille[i] = ""
+
+    #päätellään sarakkeiden muuttujat
+    muuttujat = lines[0].split(";")
+    for i in range(3, len(muuttujat)):
+        muuttujat_sarake_nimi[i] = muuttujat[i]
+
+    lines.pop(0) #poistetaan muuttujien nimeämisrivi
+    lines.pop(0) #poistetaan tietojen otsikkorivi
+    lines.pop(0) #poistetaan tietojen toinen otsikkorivi
+
+    #alustetaan vuosi loopin ulkopuolella
+    vuosi = ""
     #muodostetaan seloste
-    rivi = 0
     for line in lines:
         sarakkeet = line.split(";")
 
-        if rivi == 0:
-            for i in range(1, len(sarakkeet)):
-                sisalto = sarakkeet[i].replace('"', '').split(" ")
-                toimiala = ""
-                #for y in range(len(sarakkeet[i])):
-                #    if sisalto[y] == " ":
-                #       break
-                for sana in sisalto:
-                    if sana.lower() == muuttuja.lower():
-                        break
-                    toimiala += sana + " "
-                toimialat_sarakkeille[i] = toimiala.lstrip()
+        #päätellään vuosi
+        if sarakkeet[0] != "":
+            vuosi = sarakkeet[0]
 
-        #for i in range(len(toimialat_sarakkeille)):
-        #    seloste_toimialoille[i] = toimialat_sarakkeille.get(i)
-            for avain in toimialat_sarakkeille:
-                seloste_toimialoille[toimialat_sarakkeille.get(avain)] = ""
-
-        #sarakkeiden lukumäärän päättely
-        sarakkeet_lkm = len(sarakkeet)
+        #ajanjakso
+        ajanjakso = sarakkeet[2]
 
         #muutetaan pilkut pisteiksi, jotta muuntaminen floatiksi onnistuu
-        for i in range(1, sarakkeet_lkm):
+        for i in range(3, len(sarakkeet)):
             sarakkeet[i] = sarakkeet[i].replace(",", ".")
 
-        if rivi > 0:
-            #päätellään, onko kasvua vai laskua ja lisätään selosteeseen oikea tulostus
-            for i in range(1, sarakkeet_lkm):
-                if float(sarakkeet[i]) > 0:
-                    lisays = seloste_toimialoille.get(toimialat_sarakkeille[i])
-                    lisays += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " kasvoi " + str(sarakkeet[0]) + " neljänneksellä " + str(sarakkeet[i]) + " prosenttia. " + "\n"
-                    seloste_toimialoille[toimialat_sarakkeille[i]] = lisays
-                    #tulostus += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " kasvoi " + str(sarakkeet[0]) + " neljänneksellä " + str(sarakkeet[i]) + " prosenttia. " + "\n"
-                elif float(sarakkeet[i]) < 0:
-                    lisays = seloste_toimialoille.get(toimialat_sarakkeille[i])
-                    lisays += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " supistui " + str(sarakkeet[0]) + " neljänneksellä " + str(sarakkeet[i]) + " prosenttia. " + "\n"
-                    seloste_toimialoille[toimialat_sarakkeille[i]] = lisays
-                    #tulostus += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " supistui " + str(sarakkeet[0]) + " neljänneksellä " + str(sarakkeet[i]) + " prosenttia. " + "\n"
+        #päätellään, onko kasvua vai laskua ja lisätään selosteeseen oikea tulostus
+        for i in range(3, len(sarakkeet)):
+            lisays = ""
+            if float(sarakkeet[i]) > 0:
+                if not seloste_sarjoille.get(sarjat_sarake_nimi[i]):
+                    lisays = ""
                 else:
-                    lisays = seloste_toimialoille.get(toimialat_sarakkeille[i])
-                    lisays += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " ei kasvanut eikä supistunut " + str(sarakkeet[0]) + " neljänneksellä." + "\n"
-                    seloste_toimialoille[toimialat_sarakkeille[i]] = lisays
-                    #tulostus += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " ei kasvanut eikä supistunut " + str(sarakkeet[0]) + " neljänneksellä." + "\n"
-        rivi += 1
+                    lisays = seloste_sarjoille.get(sarjat_sarake_nimi[i])
+                lisays += "Sarjan " + str(sarjat_sarake_nimi[i]) + " " + str(muuttujat_sarake_nimi[i]) + " kasvoi " + str(ajanjakso) + "/" + str(vuosi) + "-ajanjaksolla " + str(sarakkeet[i]) + " prosenttia. " + "\n"
+                seloste_sarjoille[sarjat_sarake_nimi[i]] = lisays
+                #tulostus += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " kasvoi " + str(sarakkeet[0]) + " neljänneksellä " + str(sarakkeet[i]) + " prosenttia. " + "\n"
+            elif float(sarakkeet[i]) < 0:
+                if seloste_sarjoille.get(sarjat_sarake_nimi[i]):
+                    lisays = ""
+                else:
+                    lisays = seloste_sarjoille.get(sarjat_sarake_nimi[i])
+                lisays += "Sarjan " + str(sarjat_sarake_nimi[i]) + " " + str( muuttujat_sarake_nimi[i]) + " kasvoi " + str(ajanjakso) + "/" + str(vuosi) + "-ajanjaksolla " + str(sarakkeet[i]) + " prosenttia. " + "\n"
+                seloste_sarjoille[sarjat_sarake_nimi[i]] = lisays
+                #tulostus += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " supistui " + str(sarakkeet[0]) + " neljänneksellä " + str(sarakkeet[i]) + " prosenttia. " + "\n"
+            else:
+                if seloste_sarjoille.get(sarjat_sarake_nimi[i]):
+                    lisays = ""
+                else:
+                    lisays = seloste_sarjoille.get(sarjat_sarake_nimi[i])
+                lisays += "Sarjan " + str(sarjat_sarake_nimi[i]) + " " + str(muuttujat_sarake_nimi[i]) + " ei kasvanut tai supistunut " + str(ajanjakso) + "/" + str(vuosi) + "-ajanjaksolla " + str(sarakkeet[i]) + ". \n"
+                seloste_sarjoille[sarjat_sarake_nimi[i]] = lisays
+                #tulostus += "Toimialan " + str(toimialat_sarakkeille[i]) + " " + str(muuttuja) + " ei kasvanut eikä supistunut " + str(sarakkeet[0]) + " neljänneksellä." + "\n"
 
-    for avain in seloste_toimialoille:
-        tulostus += seloste_toimialoille.get(avain) + "\n"
+    for avain in seloste_sarjoille:
+        tulostus += seloste_sarjoille.get(avain) + "\n"
 
     with open("seloste.txt", "x") as f:
         f.write(tulostus)
@@ -79,5 +95,5 @@ def tee_seloste(data):
     return tulostus
 
 
-print(tee_seloste("C:\\Users\\ossik\\Downloads\\004_111y_2020m01.csv"))
-#print(tee_seloste("C:\\Users\\ossik\\Downloads\\004_112d_2020m01.csv"))
+
+print(tee_seloste("C:\\Users\\ossik\\Desktop\\kokeiludata_oikealla_rakenteella.csv"))
